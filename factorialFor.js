785855164218b35f2d8d2d0fee07e597fee9bd92
Mariano Mignelli <marianomignelli999@gmail.com>
2022-05-28 14:04:11 -0300

    estructuras de control Open Bootcamp

---
+++ factorialFor.js
@@ -0,0 +1,7 @@
+
+//Bucle for
+
+let i = 0
+for (let i = 0; i < 10; i++){
+console.log (i)
+}
---
+++ factorialWhile.js
@@ -0,0 +1,8 @@
+//WHILE
+
+let i = 0;
+let max = 10;
+while (i < max) {
+    console.log(i);
+    i++;
+}
